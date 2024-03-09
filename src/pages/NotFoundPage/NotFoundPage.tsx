0849cd112588c509ef89143997602a7969bde91f
vitalyvitmens <otm_@tut.by>
2024-03-09 23:47:10 +0300

    chore(src\pages\NotFoundPage\NotFoundPage.tsx): добавляет логику отображения страницы

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,26 @@
+import { Alert, Center, Button, Container, Text } from '@mantine/core'
+import { Link } from 'react-router-dom'
+
 export const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <Container mt={20}>
+      <Center
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        <Alert mb={10} radius={5}>
+          Page Not Found
+        </Alert>
+        <Text mb={10} fz={16} fw={600} style={{ color: 'red' }}>
+          Sorry this page not found
+        </Text>
+        <Button radius={5}>
+          <Link to="/">Back To Home Page</Link>
+        </Button>
+      </Center>
+    </Container>
   )
 }
