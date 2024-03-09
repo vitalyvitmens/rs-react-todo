231b51b6ac49d1c7dabadf38a5b039981fb9b696
vitalyvitmens <otm_@tut.by>
2024-03-09 18:33:50 +0300

    style: настраивает стили

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,15 @@ import '@mantine/core/styles.css'
 function App() {
   const [count, setCount] = useState(0)
   const theme = useMantineTheme()
+  const themeColor1 = theme.colors[theme.primaryColor][1]
+  const themeColor2 = theme.colors[theme.primaryColor][2]
+  const themeColor3 = theme.colors[theme.primaryColor][3]
+  const themeColor5 = theme.colors[theme.primaryColor][5]
+  const themeColor7 = theme.colors[theme.primaryColor][7]
+  const themeShadow3 = theme.shadows.md
 
   return (
-    <BackgroundImage
-      src={bgImage}
-      style={{ height: '100vh' }}
-    >
+    <BackgroundImage src={bgImage} style={{ height: '100vh' }}>
       <Center h={300}>
         <a
           href="https://github.com/vitalyvitmens/rs-react-todo"
@@ -29,14 +32,14 @@ function App() {
           <img src={todoLogo} alt="ToDo" style={{ width: '300px' }} />
         </a>
       </Center>
-      <Container c="yellow" ta="center" mt={-60}>
-        <h1>Hello, Mantine!</h1>
-        <p>Your primary color is {theme.colors[theme.primaryColor][0]}</p>
+      <Container c="yellow" ta="center" mt={-30}>
+        <Title c={themeColor3} style={{textShadow: themeShadow3}}>Hello, Mantine!</Title>
+        <Text c={themeColor3}>Your primary color is {themeColor3}</Text>
         <Button color="primary">This button has the note color</Button>
       </Container>
 
-      <Center c="#fff">
-        <Title size="4rem" c="blue">
+      <Center>
+        <Title size="4rem" c={themeColor5}>
           ToDo
         </Title>
       </Center>
@@ -47,7 +50,7 @@ function App() {
           ml={10}
           mr={10}
           variant="default"
-          bg="green"
+          bg={themeColor1}
           onClick={() => setCount((count) => count + 1)}
         >
           <Text c="brown" size="1.5rem" fw={700}>
@@ -61,12 +64,19 @@ function App() {
         ta="center"
         size="responsive"
         bg="var(--mantine-color-blue-light)"
-        c="yellow"
+        c={themeColor2}
       >
         Edit <code>src/App.tsx</code> and save to test HMR
       </Container>
       <Center h={100} p={20} mt={-15}>
-        <Text c="red" size="xl" fw={800} fs="italic" td="underline" ta="center">
+        <Text
+          c={themeColor7}
+          size="xl"
+          fw={800}
+          fs="italic"
+          td="underline"
+          ta="center"
+        >
           Click on the Vite and React logos to learn more
         </Text>
       </Center>
--- src/theme.ts
+++ src/theme.ts
@@ -3,14 +3,14 @@ import { createTheme } from '@mantine/core'
 export const theme = createTheme({
   colors: {
     note: [
-      '#FFF8E6',
-      '#FFE9B3',
+      '#FFFF00',
+      '#008000',
       '#FFD97F',
       '#FFC94C',
       '#FFB900',
-      '#CC9500',
+      '#0000FF',
       '#996F00',
-      '#664A00',
+      '#FF0000',
       '#332400',
       '#000000',
     ],
@@ -27,7 +27,7 @@ export const theme = createTheme({
   shadows: {
     xs: '0 1px 2px rgba(0, 0, 0, 0.1)',
     sm: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    md: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    md: '-3px 3px 2px black',
     lg: '0 8px 16px rgba(0, 0, 0, 0.1)',
     xl: '0 16px 32px rgba(0, 0, 0, 0.1)',
   },
