f5591fa1c815aa39aba501bfa1404f1f92dea357
vitalyvitmens <otm_@tut.by>
2024-03-09 15:41:32 +0300

    fix: меняет дислокацию MantineProvider

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import {
   Button,
   Center,
   Container,
-  MantineProvider,
   Text,
   Title,
 } from '@mantine/core'
@@ -16,60 +15,51 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <MantineProvider>
-      <BackgroundImage src={bgImage} style={{ height: '100vh' }}>
-        <Center h={350}>
-          <a
-            href="https://github.com/vitalyvitmens/rs-react-todo"
-            target="_blank"
-          >
-            <img src={todoLogo} alt="ToDo" style={{ width: '300px' }} />
-          </a>
-        </Center>
-        <Center c="#fff">
-          <Title size="4rem" c="blue">
-            ToDo
-          </Title>
-        </Center>
-        <Center color="red">
-          <Button
-            justify="center"
-            fullWidth
-            ml={10}
-            mr={10}
-            variant="default"
-            bg="green"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            <Text c="brown" size="1.5rem" fw={700}>
-              count is {count}
-            </Text>
-          </Button>
-        </Center>
-        <Container
-          w={320}
-          fw={800}
-          ta="center"
-          size="responsive"
-          bg="var(--mantine-color-blue-light)"
-          c="yellow"
+    <BackgroundImage src={bgImage} style={{ height: '100vh' }}>
+      <Center h={350}>
+        <a
+          href="https://github.com/vitalyvitmens/rs-react-todo"
+          target="_blank"
         >
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </Container>
-        <Center h={100} p={20}>
-          <Text
-            c="red"
-            size="xl"
-            fw={800}
-            fs="italic"
-            td="underline"
-            ta="center"
-          >
-            Click on the Vite and React logos to learn more
+          <img src={todoLogo} alt="ToDo" style={{ width: '300px' }} />
+        </a>
+      </Center>
+      <Center c="#fff">
+        <Title size="4rem" c="blue">
+          ToDo
+        </Title>
+      </Center>
+      <Center color="red">
+        <Button
+          justify="center"
+          fullWidth
+          ml={10}
+          mr={10}
+          variant="default"
+          bg="green"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          <Text c="brown" size="1.5rem" fw={700}>
+            count is {count}
           </Text>
-        </Center>
-      </BackgroundImage>
-    </MantineProvider>
+        </Button>
+      </Center>
+      <Container
+        w={320}
+        fw={800}
+        ta="center"
+        size="responsive"
+        bg="var(--mantine-color-blue-light)"
+        c="yellow"
+      >
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </Container>
+      <Center h={100} p={20}>
+        <Text c="red" size="xl" fw={800} fs="italic" td="underline" ta="center">
+          Click on the Vite and React logos to learn more
+        </Text>
+      </Center>
+    </BackgroundImage>
   )
 }
 
