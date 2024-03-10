89125193a1b197399e1c1fa672e0fdd8b63cdd44
vitalyvitmens <otm_@tut.by>
2024-03-10 15:51:59 +0300

    chore(src\App.tsx): добавляет роуты и провайдеры

--- src/App.tsx
+++ src/App.tsx
@@ -7,38 +7,38 @@ import { RegisterPage } from './pages/RegisterPage/RegisterPage'
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage'
 import { Offline } from './pages/Offline/Offline'
 import { NewTodo } from './pages/NewTodo/NewTodo'
-// import { AuthProvider } from './context/AuthContext'
-// import { PrivateRoute } from './components/PrivateRoute/PrivateRoute'
-// import { TodoProvider } from './context/TodoContext'
+import { AuthProvider } from './context/AuthContext'
+import { PrivateRoute } from './components/PrivateRoute/PrivateRoute'
+import { TodoProvider } from './context/TodoContext'
 
 function App() {
   return (
-    // <AuthProvider>
-    // <TodoProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            // <PrivateRoute>
-            <HomePage />
-            // </PrivateRoute>
-          }
-        >
-          <Route path="/">
-            <Route index element={<Todo />} />
-            <Route path=":todoId" element={<EditTodo />} />
-            <Route path="new" element={<NewTodo />} />
-          </Route>
-        </Route>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/offline" element={<Offline />} />
-      </Routes>
-    </BrowserRouter>
-    // </TodoProvider>
-    // </AuthProvider>
+    <AuthProvider>
+      <TodoProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            >
+              <Route path="/">
+                <Route index element={<Todo />} />
+                <Route path=":todoId" element={<EditTodo />} />
+                <Route path="new" element={<NewTodo />} />
+              </Route>
+            </Route>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/offline" element={<Offline />} />
+          </Routes>
+        </BrowserRouter>
+      </TodoProvider>
+    </AuthProvider>
   )
 }
 
