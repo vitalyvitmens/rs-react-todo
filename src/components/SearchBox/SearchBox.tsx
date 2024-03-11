235e40d7bd5672f4d89589be85d493ffb6c11026
vitalyvitmens <otm_@tut.by>
2024-03-11 15:07:36 +0300

    feat(src\components\SearchBox\SearchBox.tsx): добавляет компонент

---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,30 @@
+import { Box, Input, Button, Paper } from '@mantine/core'
+
+type SearcBoxPropType = {
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const SearchBox = ({ setSearchTerm }: SearcBoxPropType) => {
+  return (
+    <Box style={{ padding: '10px 10px', borderBottom: '1px solid #cdcdcd' }}>
+      <Paper
+        component="form"
+        style={{
+          p: '2px 4px',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Input
+          style={{ ml: 1, flex: 1 }}
+          placeholder="Search todo"
+          // inputProps={{ 'aria-label': 'search todo' }}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Button type="button" style={{ p: '10px' }} aria-label="search">
+          {/* <Search /> */}
+        </Button>
+      </Paper>
+    </Box>
+  )
+}
