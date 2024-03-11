c8f0db7ced936e20f575e851c7167a280fb54b43
vitalyvitmens <otm_@tut.by>
2024-03-11 15:07:07 +0300

    feat(src\pages\EditTodo\EditTodo.tsx): добавляет компонент

--- src/pages/EditTodo/EditTodo.tsx
+++ src/pages/EditTodo/EditTodo.tsx
@@ -1,5 +1,82 @@
+import { ChangeEvent, useCallback, useEffect, useState } from 'react'
+import SimpleMdeReact from 'react-simplemde-editor'
+import { Box, Container, Input, Text } from '@mantine/core'
+import { useParams } from 'react-router-dom'
+import { useSelectTodo } from '../../hooks/useSelectTodo'
+
 export const EditTodo = () => {
+  const { todoId } = useParams()
+  const {
+    selectTodo,
+    todo: fetchedTodo,
+    onTodoUpdate,
+    isTodoUpdated,
+    onTodoAdd,
+  } = useSelectTodo()
+  const [title, setTitle] = useState<string>(fetchedTodo?.title)
+  const [value, setValue] = useState<string>(fetchedTodo?.description)
+
+  useEffect(() => {
+    selectTodo(Number(todoId))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    selectTodo(Number(todoId))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isTodoUpdated])
+
+  const onChange = useCallback((value: string) => {
+    setValue(value)
+  }, [])
+
+  const onTitleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }, [])
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      onTodoUpdate({
+        id: Number(todoId),
+        title,
+        description: value,
+        date: new Date().toString(),
+      })
+      onTodoAdd()
+    }, 1000)
+
+    return () => {
+      clearTimeout(id)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value, title])
+
   return (
-    <div>EditTodo</div>
+    <Container style={{ overflow: 'hidden' }}>
+      <Box style={{ padding: '0 10px 20px 10px' }}>
+        <Box
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            height: '150px',
+          }}
+        >
+          <Text mb={2} variant="h5" style={{ textAlign: 'center' }}>
+            Новая заметка
+          </Text>
+          <Input.Wrapper label="Заголовок">
+            <Input
+              required
+              id="outlined-required"
+              name="title"
+              value={title}
+              onChange={onTitleChange}
+            />
+          </Input.Wrapper>
+        </Box>
+        <SimpleMdeReact value={value} onChange={onChange} />
+      </Box>
+    </Container>
   )
 }
