e0b0475211d45a9f01de146d3b202b1b655063c0
vitalyvitmens <otm_@tut.by>
2024-03-27 12:45:44 +0300

    fix(src\components\SearchInput\SearchInput.tsx): создаёт отдельную функцию в теле компонента

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,12 +1,21 @@
 import { Colors } from '../../constants/colors'
 import { Box, Divider, Input } from '@mantine/core'
 import { IconSearch } from '@tabler/icons-react'
+import React, { useState } from 'react'
 
-type SearchInputType = {
+type SearchInputProps = {
   setSearchInput: React.Dispatch<React.SetStateAction<string>>
 }
 
-export const SearchInput = ({ setSearchInput }: SearchInputType) => {
+export const SearchInput = ({ setSearchInput }: SearchInputProps) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value
+    setInputValue(newValue)
+    setSearchInput(newValue)
+  }
+
   return (
     <Box m="0 8px 10px 0">
       <Input
@@ -15,7 +24,8 @@ export const SearchInput = ({ setSearchInput }: SearchInputType) => {
         name="search"
         placeholder="Search todo"
         rightSection={<IconSearch size={24} />}
-        onChange={(e) => setSearchInput(e.target.value)}
+        value={inputValue}
+        onChange={handleInputChange}
       />
       <Divider size={2} w="100%" mt={10} color={Colors.primary} />
     </Box>
