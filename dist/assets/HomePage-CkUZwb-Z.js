cd9bbbdba896b41ad82e7703625d1f1aa05b71bb
vitalyvitmens <otm_@tut.by>
2024-03-27 22:31:28 +0300

    build: билдит проект для deploy

--- dist/assets/HomePage-CkUZwb-Z.js
+++ dist/assets/HomePage-CkUZwb-Z.js
@@ -1,4 +1,4 @@
-import{r as n,j as e,B as x,c as i,b as T,T as p,R as w,u as I,S as y,C as v,O as z}from"./index-zdD9Iunh.js";import{C as h}from"./ComponentWithSuspense-DTDeAdQA.js";import{I as E}from"./Input-ASlmub2K.js";import{c as k,D as f,C as m}from"./createReactComponent-CsUHbW4S.js";import{u as R}from"./useSelectTodo-D-8-eRnV.js";import{G as L}from"./Group-INBBAUho.js";import{B as V}from"./Button-COcmZt26.js";/**
+import{r as n,j as e,B as x,c as i,b as T,T as p,R as w,u as I,S as y,C as v,O as z}from"./index-K-nwx7xz.js";import{C as h}from"./ComponentWithSuspense-pXWPjR-J.js";import{I as E}from"./Input-CF1ytQLz.js";import{c as k,D as f,C as m}from"./createReactComponent-CQQXWyCr.js";import{u as R}from"./useSelectTodo-ujz5vwlc.js";import{G as L}from"./Group-Obir6-Kk.js";import{B as V}from"./Button-DfEQeWW0.js";/**
  * @license @tabler/icons-react v3.1.0 - MIT
  *
  * This source code is licensed under the MIT license.
--- dist/assets/useSelectTodo-D-8-eRnV.js
+++
@@ -1,1 +0,0 @@
-import{r as o,$ as t}from"./index-zdD9Iunh.js";const r=()=>o.useContext(t);export{r as u};
