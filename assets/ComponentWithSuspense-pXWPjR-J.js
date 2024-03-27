14a9682b04a3f80715aa0b99847c6f0356f988a4
vitalyvitmens <otm_@tut.by>
2024-03-27 22:31:53 +0300

    Updates

---
+++ assets/ComponentWithSuspense-pXWPjR-J.js
@@ -0,0 +1,1 @@
+import{j as s,r as o,S as r}from"./index-K-nwx7xz.js";function x({component:e,fallback:t=s.jsx(r,{}),...n}){return s.jsx(o.Suspense,{fallback:t,children:s.jsx(e,{...n})})}export{x as C};
