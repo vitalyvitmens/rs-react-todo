46b437674b76695e496ca07391f2683ca683e587
vitalyvitmens <otm_@tut.by>
2024-03-28 10:54:47 +0300

    build: билдит проект для деплой

---
+++ dist/assets/useSelectTodo-nbcWWAF6.js
@@ -0,0 +1,1 @@
+import{r as o,$ as t}from"./index-s3GS4-YT.js";const r=()=>o.useContext(t);export{r as u};
--- vite.config.ts
+++ vite.config.ts
@@ -94,6 +94,6 @@ const manifestForPlugin = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/index.html',
   plugins: [react(), VitePWA(manifestForPlugin)],
 })
