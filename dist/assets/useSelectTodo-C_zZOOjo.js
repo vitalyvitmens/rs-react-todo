d21cd13d17a2e131f31f749e651d4060685c140e
vitalyvitmens <otm_@tut.by>
2024-03-28 13:00:48 +0300

    build: билдит проект для деплой

--- dist/assets/useSelectTodo-C_zZOOjo.js
+++
@@ -1,1 +0,0 @@
-import{r as o,$ as t}from"./index-D9geeDCV.js";const r=()=>o.useContext(t);export{r as u};
--- src/sw_reg.ts
+++ src/sw_reg.ts
@@ -1,6 +1,7 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/rs-react-todo/sw.ts')
+    .register('/rs-react-todo/sw.js')
+    // .register('/rs-react-todo/sw.ts')
     .then((reg) => console.log('Service Worker registered', reg))
     .catch((err) => console.log('Service Worker not registered', err))
 }
